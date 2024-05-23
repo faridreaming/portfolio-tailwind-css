22550ac372f8459fc381d3dbd4744ffe664861ae
faridrawing <muhammad.farid.yamin@gmail.com>
2024-05-23 22:35:48 +0700

    feat: add container centering and custom theme settings in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["index.html"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: "1rem",
+    },
+    extend: {
+      colors: {
+        primary: "#a855f7",
+        dark: "#0f172a",
+      },
+      screens: {
+        "2xl": "1320px",
+      },
+    },
   },
   plugins: [],
 };
