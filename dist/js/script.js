e5807ec90bb1d171755ec3699e0c1f18972fe098
faridrawing <muhammad.farid.yamin@gmail.com>
2024-05-25 00:20:04 +0700

    feat: create dark mode logic

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,11 +2,14 @@
 window.onscroll = () => {
   const header = document.querySelector("header");
   const fixedNav = header.offsetTop;
+  const toTop = document.getElementById("to-top");
 
   if (window.scrollY > fixedNav) {
     header.classList.add("navbar-fixed");
+    toTop.classList.remove("opacity-0");
   } else {
     header.classList.remove("navbar-fixed");
+    toTop.classList.add("opacity-0");
   }
 };
 
@@ -18,3 +21,35 @@ hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("hamburger-active");
   navMenu.classList.toggle("hidden");
 });
+
+// Klik Di Luar Hamburger
+window.addEventListener("click", (e) => {
+  if (e.target !== hamburger && e.target !== navMenu) {
+    hamburger.classList.remove("hamburger-active");
+    navMenu.classList.add("hidden");
+  }
+});
+
+// Darkmode Toggle
+const darkToggle = document.getElementById("dark-toggle");
+const html = document.getElementsByTagName("html")[0];
+
+darkToggle.addEventListener("click", () => {
+  if (darkToggle.checked) {
+    html.classList.add("dark");
+    localStorage.theme = "dark";
+  } else {
+    html.classList.remove("dark");
+    localStorage.theme = "light";
+  }
+});
+
+// Ubah Posisi Toggle
+if (
+  localStorage.theme === "dark" ||
+  (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  darkToggle.checked = true;
+} else {
+  darkToggle.checked = false;
+}
